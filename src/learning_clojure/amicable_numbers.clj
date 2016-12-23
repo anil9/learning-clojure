1a7af659aa99a526b22515f59a435f841a6a564b
Andreas Nilsson <anil9@kth.se>
2016-12-23 18:00:25 +0100

    wrote comments to functions

--- src/learning_clojure/amicable_numbers.clj
+++ src/learning_clojure/amicable_numbers.clj
@@ -1,6 +1,7 @@
 (ns learning-clojure.amicable-numbers)
 (ns clojure.test.example  (:use clojure.test))
 
+;; finds all divisors of x
 (defn proper-divisors [x]
   (filter #(= (mod x %) 0) (range 1 x)))
 
@@ -11,22 +12,13 @@
   (is (= '(1) (proper-divisors 13)))
   (is (= '(1) (proper-divisors 31))))
 
-
-(defn potential-amicable [x]
+;; sums all divisors
+(defn sum-divisors [x]
   (reduce + (proper-divisors x)))
 
-(defn sum-amicable-pair [x]
-  (let [a1 (potential-amicable x) a2 (potential-amicable a1)]
-    (if (= a2 x)
-      (+ a1 a2)
-      0)))
-
-(deftest test-sum-amicable-pair
-  (is (= 504 (sum-amicable-pair 220)))
-  (is (= 504 (sum-amicable-pair 284))))
-
+;; If x is an amicable pair (x, p2) return p2, else return false
 (defn amicable-pair? [x]
-  (let [a1 (potential-amicable x) a2 (potential-amicable a1)]
+  (let [a1 (sum-divisors x) a2 (sum-divisors a1)]
     (if (and (not (= x a1))(= a2 x))
       a1
       false)))
@@ -36,9 +28,8 @@
   (is (= 220 (amicable-pair? 284)))
   (is (false? (amicable-pair? 2))))
 
-
-;(println (reduce + (map sum-amicable-pair (range 1 10000))) 2)
-(defn build-amicable-numbers-map [x value-sum]
+;; takes a number x. If its an amicable pair (x, p2) conj value-sum with {p2 x+p2}
+(defn conj-amicable-numbers-map [x value-sum]
       (if-not (contains? value-sum x)
         (let [amicable (amicable-pair? x)]
           (if-not (false? amicable)
@@ -47,21 +38,24 @@
         value-sum))
 
 (deftest test-build-map
-  (is (= {284 504} (build-amicable-numbers-map 220 {})))
-  (is (= {220 504} (build-amicable-numbers-map 284 {})))
-  (is (= {284 504} (build-amicable-numbers-map 284 {284 504}))))
-;(reduce + (vals value-sum))
+  (is (= {284 504} (conj-amicable-numbers-map 220 {})))
+  (is (= {220 504} (conj-amicable-numbers-map 284 {})))
+  (is (= {284 504} (conj-amicable-numbers-map 284 {284 504}))))
 
-;(println (amicable-numbers-sum-in-range 100))
+
+;;recursively builds a map of {amicable1 (+amicable1 amicable2} from x to range
 (defn rec-build-map [x value-map stop]
   (if (== x stop) value-map
                   (recur
-                            (+ x 1)(build-amicable-numbers-map x value-map)stop)))
+                    (+ x 1) (conj-amicable-numbers-map x value-map) stop)))
+
+;; no amicable number > 10000 allowed
 (defn filter-high-amicable [[k v]]
   (if (< 10000 k)
     [k (- v k)]
     [k v]))
 
+;; prints the result
 (println (reduce + (map last
                      (map filter-high-amicable
                           (rec-build-map 1 {} (- 10000 1))))))
