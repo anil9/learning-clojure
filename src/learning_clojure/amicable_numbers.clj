29a36935ec3e6e2b0ee2af261515b4622d96e3dc
Andreas Nilsson <anil9@kth.se>
2016-12-23 17:43:09 +0100

    amicable number passes on projecteuler

--- src/learning_clojure/amicable_numbers.clj
+++ src/learning_clojure/amicable_numbers.clj
@@ -6,7 +6,11 @@
 
 (deftest test-divisors
   (is (= '(1 2 4 71 142) (proper-divisors 284)))
-  (is (= '(1 2 4 5 10 11 20 22 44 55 110) (proper-divisors 220))))
+  (is (= '(1 2 4 5 10 11 20 22 44 55 110) (proper-divisors 220)))
+  (is (= '(1) (proper-divisors 7)))
+  (is (= '(1) (proper-divisors 13)))
+  (is (= '(1) (proper-divisors 31))))
+
 
 (defn potential-amicable [x]
   (reduce + (proper-divisors x)))
@@ -23,18 +27,42 @@
 
 (defn amicable-pair? [x]
   (let [a1 (potential-amicable x) a2 (potential-amicable a1)]
-    (if (= a2 x)
-      a1)
-    false))
+    (if (and (not (= x a1))(= a2 x))
+      a1
+      false)))
 
 (deftest test-amicable-pair
-  (is (true? (amicable-pair? 220)))
-  (is (true? (amicable-pair? 284)))
+  (is (= 284 (amicable-pair? 220)))
+  (is (= 220 (amicable-pair? 284)))
   (is (false? (amicable-pair? 2))))
 
 
 ;(println (reduce + (map sum-amicable-pair (range 1 10000))) 2)
-(defn amicable-numbers-sum-in-range [range])
+(defn build-amicable-numbers-map [x value-sum]
+      (if-not (contains? value-sum x)
+        (let [amicable (amicable-pair? x)]
+          (if-not (false? amicable)
+            (conj value-sum {amicable (+ x amicable)})
+            value-sum))
+        value-sum))
 
+(deftest test-build-map
+  (is (= {284 504} (build-amicable-numbers-map 220 {})))
+  (is (= {220 504} (build-amicable-numbers-map 284 {})))
+  (is (= {284 504} (build-amicable-numbers-map 284 {284 504}))))
+;(reduce + (vals value-sum))
 
+;(println (amicable-numbers-sum-in-range 100))
+(defn rec-build-map [x value-map stop]
+  (if (== x stop) value-map
+                  (recur
+                            (+ x 1)(build-amicable-numbers-map x value-map)stop)))
+(defn filter-high-amicable [[k v]]
+  (if (< 10000 k)
+    [k (- v k)]
+    [k v]))
+
+(println (reduce + (map last
+                     (map filter-high-amicable
+                          (rec-build-map 1 {} (- 10000 1))))))
 (run-all-tests)
