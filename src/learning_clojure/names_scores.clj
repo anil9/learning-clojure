dc8c2bddd47a898a0c7b0b6d3915bf1512bf69f7
Andreas Nilsson <anil9@kth.se>
2016-12-23 21:16:26 +0100

    solved euler problem 22: names scores

---
+++ src/learning_clojure/names_scores.clj
@@ -0,0 +1,29 @@
+(ns learning-clojure.names-scores (:use clojure.test))
+
+;; splits string x
+(defn split-fn [x]
+  (filter #(re-matches #"\w+" %) (clojure.string/split x #"\"")))
+
+(deftest split-correctly
+  (is (= '("MARY" "PATRICIA") (split-fn "\"MARY\",\"PATRICIA\"")))
+  (is (= '("MARY") (split-fn "\"MARY\""))))
+
+;; reads file into list
+(defn read-into-list [file-name]
+  (split-fn (slurp file-name)))
+
+;; calculates the alphabetical score of the name given as param
+(defn alphabetical-score [name]
+  (reduce + (map #(-(int %)64) (seq (clojure.string/upper-case name)))))
+
+(deftest test-alphabetical-score
+  (is (= 53 (alphabetical-score "COLIN"))))
+
+;; calculates the sum of all (alphabetical-score * index+1)
+(defn total-sum []
+  (let [alpha-scored-list (map alphabetical-score (sort (read-into-list "/tmp/names.txt")))]
+    (def result (reduce +' (map-indexed #(* (inc %1) %2) alpha-scored-list)))
+    result))
+
+(println (total-sum))
+(run-tests)
