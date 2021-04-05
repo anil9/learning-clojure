0260abfbf578970e98d7a389f1fd518cf8a8ec2e
andreas <anil9>
2021-04-05 18:14:45 +0200

    algorithm for encrypt/decrypt

---
+++ src/learning_clojure/encrypt.clj
@@ -0,0 +1,33 @@
+(ns learning-clojure.encrypt
+  (:require
+    [clojure.string :as str]))
+
+(defn seconds [st]
+  (if (< (count st) 2)
+    ""
+    (apply str (second st) (seconds (drop 2 st)))))
+
+(defn non-seconds [st]
+  (cond
+    (empty? st) ""
+    (<= (count st) 2) (str (first st))
+    :else (apply str (first st) (non-seconds (drop 2 st)))))
+
+(defn encrypt [st n]
+  (if (or (str/blank? st) (<= n 0))
+    st
+    (encrypt (apply str (seconds st) (non-seconds st)) (dec n))))
+
+(defn two-part-str [st]
+  (let [st st
+        half-len (quot (count st) 2)]
+    (list (subs st 0 half-len) (subs st half-len))))
+
+(defn decrypt [st n]
+  (if (or (str/blank? st) (<= n 0))
+    st
+    (let [first-half (first (two-part-str st))
+          second-half (second (two-part-str st))]
+      (if (= (count first-half) (count second-half))
+        (decrypt (apply str (interleave second-half first-half)) (dec n))
+        (decrypt (apply str (concat (interleave second-half first-half) [(last second-half)])) (dec n))))))
---
+++ test/learning_clojure/encrypt_test.clj
@@ -0,0 +1,50 @@
+(ns learning-clojure.encrypt-test
+  (:require [clojure.test :refer :all])
+  (:require [learning-clojure.encrypt :refer :all]))
+
+(deftest tests-seconds
+  (testing "seconds tests"
+    (is (= "" (seconds "")))
+    (is (= "b" (seconds "ab")))
+    (is (= "2468" (seconds (range 1 10))))
+    ))
+
+(deftest tests-non-seconds
+  (testing "non-seconds tests"
+    (is (= "" (non-seconds "")))
+    (is (= "a" (non-seconds "ab")))
+    (is (= "13579" (non-seconds (range 1 10))))
+    ))
+
+(deftest sample-tests-encrypt
+  (testing "Encryption tests"
+    (is (= "" (encrypt "" 5)))
+    (is (= nil (encrypt nil 2)))
+    (is (= "This is a test!" (encrypt "This is a test!" 0)))
+    (is (= "hsi  etTi sats!" (encrypt "This is a test!" 1)))
+    (is (= "s eT ashi tist!" (encrypt "This is a test!" 2)))
+    (is (= " Tah itse sits!" (encrypt "This is a test!" 3)))
+    (is (= "This is a test!" (encrypt "This is a test!" 4)))
+    (is (= "This is a test!" (encrypt "This is a test!" -1)))
+    (is (= "hskt svr neetn!Ti aai eyitrsig"
+           (encrypt "This kata is very interesting!" 1)))))
+
+(deftest tests-two-part-str
+  (testing "Dividing a string in two parts"
+    (is (= '("This is" " a test") (two-part-str "This is a test")))
+    (is (= '("This is" " a test!") (two-part-str "This is a test!")))
+    ))
+
+(deftest sample-tests-decrypt
+  (testing "Decryption tests"
+    (is (= "This is a test!" (decrypt "This is a test!" 0)))
+    (is (= "This is a test" (decrypt "hsi  etTi sats" 1)))
+    (is (= "This is a test!" (decrypt "hsi  etTi sats!" 1)))
+
+
+    (is (= "This is a test!" (decrypt "s eT ashi tist!" 2)))
+    (is (= "This is a test!" (decrypt " Tah itse sits!" 3)))
+    (is (= "This is a test!" (decrypt "This is a test!" 4)))
+    (is (= "This is a test!" (decrypt "This is a test!" -1)))
+    (is (= "This kata is very interesting!"
+           (decrypt "hskt svr neetn!Ti aai eyitrsig" 1)))))
