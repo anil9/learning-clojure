954be7bd8b599668f030348d2df2fae0717da1f8
andreas <anil9>
2021-04-12 19:51:13 +0200

    Working version that needs a bit of refactoring

--- src/learning_clojure/codewars/playing_with_digits.clj
+++ src/learning_clojure/codewars/playing_with_digits.clj
@@ -1,11 +1,7 @@
 (ns learning-clojure.codewars.playing-with-digits)
 
-(defn digits [n]
-  (loop [n n
-         res ()]
-    (if (< n 10)
-      (conj res n)
-      (recur (quot n 10) (conj res (rem n 10))))))
+(set! *warn-on-reflection* true)
+(set! *unchecked-math* true)
 
 (defn pow [item exp]
   (long (Math/pow item exp)))
@@ -13,19 +9,239 @@
 (defn inc-pow [p idx item]
   (pow item (+ p idx)))
 
+
+(defn digits [n]
+  (loop [n n
+         res ()]
+    (if (< n 10)
+      (conj res n)
+      (recur (quot n 10) (conj res (rem n 10))))))
+
+
 (defn dig-pow [n p]
   "want to find an int k, if it exists, such as the sum of digits of n
   taken to the successive powers of p is equal to k * n"
   (let [sum (->> (digits n)
                  (map-indexed (partial inc-pow p))
-                 (reduce +)
-                 )]
+                 (reduce +))]
+
     (if (zero? (mod sum n))
       (quot sum n)
       -1)))
-
+; ---------------------------EUREKA
 (def eureka-start-exponent 1)
 
 (defn sum-dig-pow [from to]
   (->> (range from (inc to))
        (filter (fn [x] (pos? (dig-pow x eureka-start-exponent))))))
+; ---------------------------prime
+
+(defn prime? [x]
+  (nil? (->>
+          (range 2 (inc (Math/sqrt x)))
+          (some #(= 0 (mod x %))))))
+
+
+; ---------------------------power-sum-dig-term
+
+
+
+(defn sum-digits [n]
+  (apply + (digits n)))
+
+
+(defn log-n-div-log-sum-digits [n sum-digits]
+  (Math/round (/ (Math/log n) (Math/log sum-digits))))
+
+(defn is-valid? [n]
+  (if (= n 81) true
+               (let [sum-digits (sum-digits n)
+                     exponent (log-n-div-log-sum-digits n sum-digits)]
+                 (and
+                   (> n 10)
+                   (> sum-digits 1)
+                   (> exponent 2)
+                   (= n (long (Math/pow sum-digits
+                                        exponent)))))))
+
+
+
+(defn power-sum-dig-term-wrong-way [n]
+  (last (->> (range)
+             (filter is-valid?)
+             (filter some?)
+             (take n))))
+
+;-----------------------------------
+(defn digits [n]
+  (loop [n n
+         res ()]
+    (if (< n 10)
+      (conj res n)
+      (recur (quot n 10) (conj res (rem n 10))))))
+
+(defn sum-digits [n]
+  (apply + (digits n)))
+
+(defn log-n-div-log-sum-digits [n sum-digits]
+  (Math/round (/ (Math/log n) (Math/log sum-digits))))
+
+(defn has-power-exp? [n]
+  (let [sum-digits (sum-digits n)]
+    (if (< sum-digits 2)
+      false
+      (loop [n n
+             exp 2]
+        (let [res (bigdec (.pow (bigdec sum-digits) exp))]
+          (cond
+            (= res n) true
+            (> res n) false
+            :else (recur n (inc exp))))))))
+
+(defn is-valid-with-precision? [n]
+  (if (< n 80)
+    false
+    (has-power-exp? n)))
+
+
+(defn is-valid? [n]
+  (if (< n 80)
+    false
+    (let [sum-digits (sum-digits n)
+          exponent (log-n-div-log-sum-digits n sum-digits)]
+      (and
+        (> sum-digits 1)
+        (= n (bigint (Math/pow sum-digits
+                               exponent)))))))
+
+
+
+(defn lots-pows [need-precision b]
+  (cond
+    (true? need-precision)
+    (->> (range 2 30)
+         (map (fn [e] (bigdec (.pow (bigdec b) e))))
+         (filter is-valid-with-precision?)
+         (filter some?))
+    :else
+    (->> (range 2 40)
+         (map (fn [e] (bigint (Math/pow b e))))
+         (filter is-valid?)
+         (filter some?))))
+
+
+(defn power-sum-dig-term [n]
+  (let [precision-mode (> n 31)]
+    (if (true? precision-mode)
+      (bigint (last
+                (take n
+                      (sort
+                        (->> (range 2 340)
+                             (mapcat (partial lots-pows precision-mode))
+                             (distinct))))))
+      (bigint (last
+                (take n
+                      (sort
+                        (->> (range 2 350)
+                             (mapcat (partial lots-pows precision-mode))
+                             (distinct)))))))))
+
+
+(defn power-sum-dig-term [n]
+  (take n
+        (sort
+          (->> (range 2 300)
+               (mapcat (partial lots-pows true))
+               (distinct)))))
+
+
+(defn find-faulty-num [max]
+    (loop [i 2
+           prev-res (power-sum-dig-term (dec i))
+           curr-res (power-sum-dig-term i)
+           numbers [prev-res]]
+      (cond
+        (= max i) (conj numbers curr-res)
+        (<= curr-res prev-res) numbers
+        :else (recur (inc i) curr-res (power-sum-dig-term (inc i)) (conj numbers curr-res)))))
+
+
+
+;(take (+ n 10000)))))))
+
+
+
+
+; LÖSNING:
+(comment (defn digits [n]
+           (loop [n n
+                  res ()]
+             (if (< n 10)
+               (conj res n)
+               (recur (quot n 10) (conj res (rem n 10))))))
+
+         (defn sum-digits [n]
+           (apply + (digits n)))
+
+         (defn log-n-div-log-sum-digits [n sum-digits]
+           (Math/round (/ (Math/log n) (Math/log sum-digits))))
+
+         (defn has-power-exp? [n]
+           (let [sum-digits (sum-digits n)]
+             (if (< sum-digits 2)
+               false
+               (loop [n n
+                      exp 2]
+                 (let [res (bigdec (.pow (bigdec sum-digits) exp))]
+                   (cond
+                     (= res n) true
+                     (> res n) false
+                     :else (recur n (inc exp))))))))
+
+         (defn is-valid-with-precision? [n]
+           (if (< n 80)
+             false
+             (has-power-exp? n)))
+
+
+         (defn is-valid? [n]
+           (if (< n 80)
+             false
+             (let [sum-digits (sum-digits n)
+                   exponent (log-n-div-log-sum-digits n sum-digits)]
+               (and
+                 (> sum-digits 1)
+                 (= n (bigint (Math/pow sum-digits
+                                        exponent)))))))
+
+
+
+         (defn lots-pows [need-precision b]
+           (cond
+             (true? need-precision)
+             (->> (range 2 40)
+                  (map (fn [e] (bigdec (.pow (bigdec b) e))))
+                  (filter is-valid-with-precision?)
+                  (filter some?))
+             :else
+             (->> (range 2 40)
+                  (map (fn [e] (bigint (Math/pow b e))))
+                  (filter is-valid?)
+                  (filter some?))))
+
+
+         (defn power-sum-dig-term [n]
+           (let [precision-mode (> n 31)]
+             (if (true? precision-mode)
+               (bigint (last
+                         (take n
+                               (sort
+                                 (->> (range 2 150)
+                                      (mapcat (partial lots-pows precision-mode))
+                                      (distinct))))))
+               (bigint (last
+                         (take n
+                               (sort
+                                 (->> (range 2 150)
+                                      (mapcat (partial lots-pows precision-mode))
+                                      (distinct))))))))))
--- test/learning_clojure/codewars/playing_with_digits_test.clj
+++ test/learning_clojure/codewars/playing_with_digits_test.clj
@@ -1,32 +1,34 @@
 (ns learning-clojure.codewars.playing-with-digits-test
   (:require [clojure.test :refer :all])
-  (:require [learning-clojure.codewars.playing-with-digits :refer [dig-pow]])
-  (:require [learning-clojure.codewars.playing-with-digits :refer [digits]])
-  (:require [learning-clojure.codewars.playing-with-digits :refer [inc-pow]])
-  (:require [learning-clojure.codewars.playing-with-digits :refer [sum-dig-pow]])
-  )
+  (:require [learning-clojure.codewars.playing-with-digits :refer [dig-pow digits
+                                                                   inc-pow
+                                                                   sum-dig-pow
+                                                                   power-sum-dig-term
+                                                                   is-valid?
+                                                                   prime?]]))
+
 
 (deftest dig-pow-test
   (is (= (dig-pow 89 1) 1))
   (is (= (dig-pow 92 1) -1))
-  (is (= (dig-pow 135 1) 1))
-  )
+  (is (= (dig-pow 135 1) 1)))
+
 
 (deftest digits-test
   (is (= [4 5] (digits 45)))
   (is (= [1 2 3] (digits 123)))
   (is (= [1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9] (digits 123456789123456789)))
-  (is (= [1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9] (digits 123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789)))
+  (is (= [1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9] (digits 123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789))))
 
-  )
+
 
 (deftest inc-pow-test
   (is (= '(1 3 16) (map-indexed (partial inc-pow 0) '(2 3 4))))
   (is (= '(2 9 64) (map-indexed (partial inc-pow 1) '(2 3 4))))
   (is (= '(4 27 256) (map-indexed (partial inc-pow 2) '(2 3 4))))
-  (is (= '(5764801 134217728 3486784401) (map-indexed (partial inc-pow 8) '(7 8 9))))
+  (is (= '(5764801 134217728 3486784401) (map-indexed (partial inc-pow 8) '(7 8 9)))))
 
-  )
+
 
 (deftest sum-dig-pow-test
   (is (= (range 1 10) (sum-dig-pow 1 10)))
@@ -35,5 +37,37 @@
   (is (= '() (sum-dig-pow 90 100)))
   (is (= '(135) (sum-dig-pow 90 150)))
   (is (= '(89 135) (sum-dig-pow 50 150)))
-  (is (= '(89 135) (sum-dig-pow 10 150)))
-  )
+  (is (= '(89 135) (sum-dig-pow 10 150))))
+
+
+(deftest power-sum-dig-term-test
+  (is (= 81 (power-sum-dig-term 1)))
+  (is (= 512 (power-sum-dig-term 2)))
+  (is (= 2401 (power-sum-dig-term 3)))
+  (is (= 4913 (power-sum-dig-term 4)))
+  (is (= 614656 (power-sum-dig-term 10)))
+  (is (= 27512614111 (power-sum-dig-term 21)))
+  (is (= 52523350144 (power-sum-dig-term 22)))
+  (is (= 52523350144 (power-sum-dig-term 22)))
+  (is (= 68719476736 (power-sum-dig-term 23)))
+  (is (= 248155780267521 (power-sum-dig-term 30))))
+
+
+(deftest is-valid?-test
+  (is (= true (is-valid? 81)))
+  (is (= false (is-valid? 82)))
+  (is (= true (is-valid? 512)))
+  (is (= true (is-valid? 2401)))
+  (is (= true (is-valid? 4913)))
+  (is (= true (is-valid? 5832)))
+  (is (= true (is-valid? 17576)))
+  (is (= true (is-valid? 19683)))
+  (is (= true (is-valid? 234256)))
+  (is (= false (is-valid? 234257))))
+  ;(is (= true (is-valid? 150094635296999121))))
+
+(deftest prime?-test
+  (is (= true (prime? 3)))
+  (is (= false (prime? 4)))
+  (is (= true (prime? 11))))
+
