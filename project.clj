83473d3dff587b18c3cdadb439f4f926a4d1ce5f
andreas <anil9>
2022-07-15 21:18:31 +0200

    Bump clojure version

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"], [proto-repl "0.3.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"], [proto-repl "0.3.1"]]
   :repl-options {:init-ns core.clj})
