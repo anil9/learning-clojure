1500a311fccf875de9814c0dfb623935ff7171de
andreas <anil9@kth.se>
2022-08-12 14:01:26 +0200

    Solved another Codewars task

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.11.1"], [proto-repl "0.3.1"]]
-  :repl-options {:init-ns core.clj})
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [proto-repl "0.3.1"]
+                 [org.clojure/tools.trace "0.7.11"]])
---
+++ test/learning_clojure/codewars/readability_is_king_test.clj
@@ -0,0 +1,29 @@
+(ns learning-clojure.codewars.readability-is-king-test
+  (:require [clojure.test :refer :all])
+  (:require [learning-clojure.codewars.readability-is-king
+              :refer [words-per-sentence count-syllables flesch-kincaid]]))
+
+(deftest words-per-sentence-test
+  (is (= 4 (count (words-per-sentence "The turtle is leaving.")))))
+
+(deftest num-syllables-test
+  (is (= 2 (count-syllables "leaving")))
+  (is (= 1 (count-syllables "hi")))
+  (is (= 2 (count-syllables "bodyguard")))
+  (is (= 1 (count-syllables "not")))
+  (is (= 1 (count-syllables "be")))
+  (is (= 1 (count-syllables "That"))))
+
+  
+(defn =? [a b] (= (format "%.2f" a) (format "%.2f" b)))
+
+(def cases [ 
+            [ "The turtle is leaving." 3.67]
+            [ "A good book is hard to find." -1.06]
+            [ "To be or not to be. That is the question." -0.66]
+            [ "Oh no! The lemming is falling." 1.31]
+            [ "Do not cut your fingers as your katana is getting sharper! Be gentle." 4.19]])
+   
+(deftest deterministic-tests
+  (doseq [[text val] cases]
+         (testing text (is (=? (flesch-kincaid text) val)))))
